1342cab4c8e22529563feaea9f9c357e129aafa3
Hossam Khaled <hossam.khaled@nahdetmisr.com>
2025-07-28 13:43:06 +0300

    refactor: Simplify view engine file handling; use hashed filename for storage

--- includes/helpers/view.php
+++ includes/helpers/view.php
@@ -29,27 +29,28 @@ if (!function_exists('view_engine')) {
     function view_engine(string $view, array $data = [])
     {
         extract($data);
-        $file_path = explode('/', $view);
-        $file_name = end($file_path);
-        $save_to_storage = base_path('storage/views/' . $file_name);
+        // $file_path = explode('/', $view);
+        // $file_name = end($file_path);
+        $file_hash_name = md5($view);
+        $save_to_storage = base_path('storage/views/' . $file_hash_name . '.php');
         // if (! file_exists($save_to_storage)) {
         $file = file_get_contents($view);
         $file = str_replace('{{', '<?php echo ', $file);
         $file = str_replace('}}', ' ;?>', $file);
-$file = str_replace('@php', '<?php ', $file);
+        $file = str_replace('@php', '<?php ', $file);
         $file = str_replace('@endphp', ' ?>', $file);
 
-$file = preg_replace('/@if\((.*)\)/i', '<?php if($1): ?>', $file);
-$file = preg_replace('/@elssif\((.*)\)/i', '<?php elssif($1): ?>', $file);
-$file = preg_replace('/@eles/i', '<?php else: ?>', $file);
-$file = preg_replace('/@endif/i', '<?php endif; ?>', $file);
+        $file = preg_replace('/@if\((.*)\)/i', '<?php if($1): ?>', $file);
+        $file = preg_replace('/@elssif\((.*)\)/i', '<?php elssif($1): ?>', $file);
+        $file = preg_replace('/@eles/i', '<?php else: ?>', $file);
+        $file = preg_replace('/@endif/i', '<?php endif; ?>', $file);
 
-$file = preg_replace('/@foreach\((.*?) as (.*?)\)+/i', '<?php foreach($1 as $2): ?>', $file);
-$file = preg_replace('/@endforeach/i', '<?php endforeach; ?>', $file);
-file_put_contents($save_to_storage, $file);
-// }
+        $file = preg_replace('/@foreach\((.*?) as (.*?)\)+/i', '<?php foreach($1 as $2): ?>', $file);
+        $file = preg_replace('/@endforeach/i', '<?php endforeach; ?>', $file);
+        file_put_contents($save_to_storage, $file);
+        // }
 
 
-include $save_to_storage;
-}
+        include $save_to_storage;
+    }
 }
