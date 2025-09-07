09df26a16e7e5dc8acf2c5709b6047ae12b6fa56
Hossam <hossam.hmiq@gmail.com>
2025-09-07 20:29:37 +0300

    Add color mode toggler and implement blog layout
    - Introduced a new JavaScript file for color mode toggling, allowing users to switch between light, dark, and auto themes.
    - Created a new home page layout with featured blog posts and a sidebar for recent posts and archives.
    - Added a footer layout with Bootstrap styling and links.
    - Implemented a header layout that supports language switching and includes a responsive navigation bar.
    - Ensured RTL support for Arabic language and integrated Bootstrap's CSS for both LTR and RTL layouts.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 include 'admin.php';
 // route_get('home', 'home');
-route_get('/','home');
-route_get('users','user');
+route_get('/', 'front.home');
+route_get('users', 'user');
 route_get('lang', 'controllers.set_lang');
 route_post('upload', 'controllers.upload');
